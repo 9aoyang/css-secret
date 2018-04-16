37d471bc46f1cd18b7fff13edee9ffd86b1aac28
9aoyang <9aoyang@gmail.com>
2018-04-16 20:33:25 +0800

    38. code-optimize

--- 7. structure && layout/38. styling-sibling-count/index.js
+++ 7. structure && layout/38. styling-sibling-count/index.js
@@ -1,13 +1,10 @@
-function $$(expr, con) { return [].slice.call((con || document).querySelectorAll(expr)); }
-
-let colors = [
-    '#D6E055', // Agave
-    '#082323', '#E6E2AF', '#A7A37E', '#EFECCA', '#046380', // Sandy stone beach
-   	'#1C171D', '#FEE169', '#CDD452', '#F9722E', '#C9313D', // Sushi Maki
-   	'#2E95A3', '#50B8B4', '#C6FFFA', '#E2FFA8', // Agave
-  ],
-  palette = document.querySelector('.palette'),
-  template = palette.firstElementChild;
+const colors = [
+  '#D6E055', // Agave
+  '#082323', '#E6E2AF', '#A7A37E', '#EFECCA', '#046380', // Sandy stone beach
+  '#1C171D', '#FEE169', '#CDD452', '#F9722E', '#C9313D', // Sushi Maki
+  '#2E95A3', '#50B8B4', '#C6FFFA', '#E2FFA8', // Agave
+];
+const palette = document.querySelector('.palette');
 
 function addColor(template) {
   const li = template.cloneNode(true);
@@ -17,12 +14,12 @@ function addColor(template) {
   palette.insertBefore(li, template.nextSibling);
 }
 
-palette.onclick = function (evt) {
+palette.onclick = (evt) => {
   const button = evt.target;
 
-  if (button.className == 'add') {
+  if (button.className === 'add') {
     addColor(button.parentNode.parentNode);
-  } else if (button.className == 'delete') {
+  } else if (button.className === 'delete') {
     const li = button.parentNode.parentNode;
     li.parentNode.removeChild(li);
   }
