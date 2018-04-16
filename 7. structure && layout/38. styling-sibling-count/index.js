da36a0800e9e8d5d80e85b1c76d6e749c29696ef
9aoyang <9aoyang@gmail.com>
2018-04-16 20:24:07 +0800

    styling-sibling-count

---
+++ 7. structure && layout/38. styling-sibling-count/index.js
@@ -0,0 +1,29 @@
+function $$(expr, con) { return [].slice.call((con || document).querySelectorAll(expr)); }
+
+let colors = [
+    '#D6E055', // Agave
+    '#082323', '#E6E2AF', '#A7A37E', '#EFECCA', '#046380', // Sandy stone beach
+   	'#1C171D', '#FEE169', '#CDD452', '#F9722E', '#C9313D', // Sushi Maki
+   	'#2E95A3', '#50B8B4', '#C6FFFA', '#E2FFA8', // Agave
+  ],
+  palette = document.querySelector('.palette'),
+  template = palette.firstElementChild;
+
+function addColor(template) {
+  const li = template.cloneNode(true);
+  const color = colors.pop();
+  colors.unshift(color);
+  li.style.background = color;
+  palette.insertBefore(li, template.nextSibling);
+}
+
+palette.onclick = function (evt) {
+  const button = evt.target;
+
+  if (button.className == 'add') {
+    addColor(button.parentNode.parentNode);
+  } else if (button.className == 'delete') {
+    const li = button.parentNode.parentNode;
+    li.parentNode.removeChild(li);
+  }
+};
